1d68008ad6ba6d0adbff759a4b647c5e6caba9d5
Mostafa Elsayad <mostafaelsayad745@gmail.com>
2024-09-12 14:05:30 +0300

    UserServiceAdded

---
+++ src/toBee_microservices/Services/User/UserService.API/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UserService.API.Models;
+
+namespace UserService.API.Data
+{
+	public class AppDbContext : IdentityDbContext<ApplicationUser>
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+		}
+
+	}
+}
---
+++ src/toBee_microservices/Services/User/UserService.API/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UserService.API.Models
+{
+	public class ApplicationUser : IdentityUser
+	{
+		public string Role { get; set; } = string.Empty; // "Professional", "Student", "Parent"
+		public bool FocusModeStatus { get; set; }
+		public DateTime JoinDate { get; set; }
+	}
+}
